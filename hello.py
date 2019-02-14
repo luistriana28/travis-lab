f25647f5392ef8b25bf2b9689dd70ed57868cae5
Luis Triana <lt@jarsa.mx>
2019-02-13 20:56:20 -0800

    [REF]hello : Modify file for testing travis

--- hello.py
+++ hello.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python
 
-suma = 2 + 4
+suma = 2+4
 
 def resultado(suma):
-    if suma == 6:
+    if not suma=6:
         return "la suma es correcta"
-    else:
-        return "la suma es:" + str(suma)
-    return suma
-
-def test_suma():
-    assert resultado(7) != 6
+        return "la suma es:"+str(suma)
