2e4e1db536ff912ff9b189753c338ac8cefc7636
Luis M. Triana <luistriana.28@gmail.com>
2020-04-08 13:10:05 -0500

    [FIX]hello.py, Change the misspelling

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
-suma = 2+4
+suma = 2 + 4
 
 def resultado(suma):
     if not suma == 6:
-        return "la suma es correcta"
-    return "la suma es:"+str(suma)
+        return "The sum is not correct"
+    return "The sum is:"+str(
+        suma)
